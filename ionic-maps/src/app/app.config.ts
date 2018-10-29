c804434ff6b0eef0df62312fb0f7593687736cb7
Luong Dinh <ldinh195@gmail.com>
2018-10-29 18:47:21 -0500

    Add files via upload

---
+++ ionic-maps/src/app/app.config.ts
@@ -0,0 +1,11 @@
+export const appconfig = {
+    //production: false,
+    firebase: {
+        apiKey: "AIzaSyCtK2X-jXnnFVOYefARlrW2nX5-uzcgb74",
+        authDomain: "my-location-d3e9e.firebaseapp.com",
+        databaseURL: "https://my-location-d3e9e.firebaseio.com",
+        projectId: "my-location-d3e9e",
+        storageBucket: "my-location-d3e9e.appspot.com",
+        messagingSenderId: "210312637203"
+    }
+  };
